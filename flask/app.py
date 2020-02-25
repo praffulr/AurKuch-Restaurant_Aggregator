18549252eaab3cf304458eb8de6a25b6e28e4538
praffulr <cs1170369@iitd.ac.in>
2020-02-25 12:00:55 +0530

    Mind the filepaths. Updated database for users with loction(text).

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, request
 import psycopg2
 # Connect to the database
-db = 'host=127.0.0.1  dbname=project user=postgres password=postgres'
+db = 'host=127.0.0.1  dbname=project user=prafful password=pandu'
 conn = psycopg2.connect(db)
 cur = conn.cursor()
 
@@ -10,46 +10,93 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-    cur.execute(
-    """
-        SELECT name FROM restaurants
-        ORDER BY random()
-        LIMIT 25
-    """)
-    rows = cur.fetchall()
+    # cur.execute("""SELECT name FROM restaurantsORDER BY random()LIMIT 25""")
+    # rows = cur.fetchall()
 
-    return render_template("base.html", rows=rows)
+    return render_template("users.html")
+
+@app.route("/homepage", methods = ['POST'])
+def home():
+    if request.method == 'POST':
+        username = request.form.get("Username")
+        password = request.form.get("Password")
+        cur.execute(
+        """
+            CREATE VIEW current_user_details as
+            select * from users
+            where users.username = \'{}\' and users.password = \'{}\'
+        """.format(username, password)
+        )
+        cur.execute(
+        """
+            WITH locs as
+            (
+                select distinct(locations.location) as location
+                from locations, current_user_details
+                  where locations.listed_in_city = current_user_details.location
+            )
+            SELECT *
+            from restaurants LEFT OUTER JOIN locs
+              on restaurants.location = locs.location
+            ORDER BY restaurants.votes desc
+            LIMIT 25
+        """
+        )
+        rows = cur.fetchall()
+        return render_template("base.html", rows = rows)
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 @app.route("/locations", methods=['GET', 'POST'])
 def locations():
     if request.method == 'POST':
-        loc = request.form.get('Id')
+        loc = request.form.get('loc_Id')
+        #city = request.form.get('City')
         cur.execute(
         """
-            SELECT *  FROM locations
-            WHERE location = \'{}\'
+            SELECT *  FROM RESTAURANTS
+            where location = \'{}\'
             LIMIT 25
-        """.format(loc))
+        """.format(loc)
+        )
         rows = cur.fetchall()
-    
+        # return format(loc)
         return render_template("base.html", rows=rows)
 
+
 @app.route("/restaurants", methods=['GET', 'POST'])
-def restaurantSearch():
+def rests():
     if request.method == 'POST':
-        rest = request.form.get('Rest_Id')          #Change the name
+        rest = request.form.get('restaurant')
         cur.execute(
-            """
-                SELECT dish_liked
-                FROM dishes, restaurants, rest_dish_links
-                WHERE rest_dish_links.rest_id = restaurants.restaurant_id
-                    AND restaurants.name = \'{}\' 
-                    AND rest_dish_links.dish_id = dishes.dish_id
-                order by dish_liked asc;
-            """.format(rest)
-            )
+        """
+            SELECT *  FROM restaurants
+            where restaurant.name =\'{}\'
+            LIMIT 25
+        """.format(rest)
+        )
         rows = cur.fetchall()
-    
+        # return format(loc)
         return render_template("base.html", rows=rows)
+
+# @app.route("/restaurants", methods=['GET', 'POST'])
+# def restaurantSearch():
+#     if request.method == 'POST':
+#         rest = request.form.get('Rest_Id')          #Change the name
+#         cur.execute(
+#             """
+#                 SELECT dish_liked
+#                 FROM dishes, restaurants, rest_dish_links
+#                 WHERE rest_dish_links.rest_id = restaurants.restaurant_id
+#                     AND restaurants.name = \'{}\'
+#                     AND rest_dish_links.dish_id = dishes.dish_id
+#                 order by dish_liked asc;
+#             """.format(rest)
+#             )
+#         rows = cur.fetchall()
+#
+#         return render_template("base.html", rows=rows)
 if __name__ == "__main__":
     app.run(host="localhost", port=5001, debug=True)
--- tableCreation.py
+++ tableCreation.py
@@ -2,13 +2,12 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Feb 18 16:32:02 2020
-
 @author: lalithsrinivas
 """
 
 import pandas as pd
 
-original_file = pd.read_csv('../zomato.csv', index_col=False)
+original_file = pd.read_csv('./database/zomato.csv', index_col=False)
 all_columns = original_file.columns
 rest_dict = {}
 loc_table = pd.DataFrame()
@@ -35,6 +34,9 @@ rest_cuisine_links = pd.DataFrame()
 rest_id_link2 = []
 cuisine_id_link2 = []
 
+
+user_loc_dict = {}
+
 for i in all_columns:
     if i == 'approx_cost(for two people)':
         cost_col = []
@@ -57,8 +59,13 @@ for i in all_columns:
             else:
                 rate_list.append('-1')
         restaurant_table[i] = rate_list
-
+    elif i == 'location':
+        restaurant_table[i] = original_file[i]
+        loc_table[i] = original_file[i]
     elif i in loc_columns:
+        for j in original_file[i]:
+            if user_loc_dict.get(j) == None:
+                user_loc_dict[j] = 1
         loc_table[i] = original_file[i]
     elif i in restaurant_col:
         restaurant_table[i] = original_file[i]
@@ -79,7 +86,7 @@ for i in all_columns:
                             cuisine_id_link2.append(len(cuisines_list) - 1)
                 rest_dict[original_file['name'][iter]+original_file['address'][iter]] = 1
             iter = iter + 1
-                
+
 
         cuisines_table[i] = cuisines_list
         rest_cuisine_links['restaurant_id'] = rest_id_link2
@@ -107,9 +114,23 @@ for i in all_columns:
         rest_dish_links['restaurant_id'] = rest_id_link1
         rest_dish_links['dish_id'] = dish_id_link1
         rest_dict = {}
-# loc_table = loc_table.drop_duplicates()
-# restaurant_table = restaurant_table.drop_duplicates()
-# dishes_table = dishes_table.drop_duplicates()
+
+
+users_file = pd.read_csv('./database/MOCKDATA.csv', index_col=False)
+users_columns = users_file.columns
+users_table = pd.DataFrame()
+
+user_loc_list = list(user_loc_dict)
+for i in users_columns:
+    if i == 'location':
+        city_list = []
+        for j in users_file[i]:
+            city_list.append(user_loc_list[j%(len(user_loc_list))])
+        users_table[i] = city_list
+    else:
+        users_table[i] = users_file[i]
+
+
 
 loc_table = loc_table.drop_duplicates(ignore_index = True)
 restaurant_table = restaurant_table.drop_duplicates(subset=['address', 'name'])
@@ -118,10 +139,15 @@ dishes_table = dishes_table.drop_duplicates()
 rest_cuisine_links = rest_cuisine_links.drop_duplicates()
 rest_dish_links = rest_dish_links.drop_duplicates()
 
+users_table = users_table.drop_duplicates(subset = ['username'], ignore_index = True)
 
-loc_table.to_csv('../location.csv', index_label='location_id')
-restaurant_table.to_csv('../restaurant.csv', index_label='restaurant_id')
-cuisines_table.to_csv('../cuisines.csv', index_label = 'cuisine_id')
-dishes_table.to_csv('../dishes.csv', index_label='dish_id')
-rest_cuisine_links.to_csv('../restaurant_cuisine_links.csv', index_label = 'link_id')
-rest_dish_links.to_csv('../restaurant_dish_links.csv', index_label = 'link_id')
+
+loc_table.to_csv('./database/location.csv', index_label='location_id')
+restaurant_table.to_csv('./database/restaurant.csv', index_label='restaurant_id')
+cuisines_table.to_csv('./database/cuisines.csv', index_label = 'cuisine_id')
+dishes_table.to_csv('./database/dishes.csv', index_label='dish_id')
+rest_cuisine_links.to_csv('./database/restaurant_cuisine_links.csv', index_label = 'link_id')
+rest_dish_links.to_csv('./database/restaurant_dish_links.csv', index_label = 'link_id')
+
+
+users_table.to_csv('./database/userdata.csv', index = False)
